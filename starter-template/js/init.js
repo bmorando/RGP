0e800b077fb370ca01437525efa4041b95fac96e
Brigette Morando <bibimorando13@yahoo.com>
2016-08-18 12:10:12 -0700

    added carousel and paralax

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -4,4 +4,14 @@
     $('.button-collapse').sideNav();
 
   }); // end of document ready
+
+  $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
 })(jQuery); // end of jQuery name space
+
